337c83f3343111552d6f81a0e0c07f3d51558275
Ryan Kim <rdhkim0@gmail.com>
2021-10-17 22:03:21 -0400

    Add initial scripts to draw board & pieces

---
+++ ChessEngine.py
@@ -0,0 +1,23 @@
+"""
+This class is responsible for storing all the information about the current state of a chess game.
+It will also be responsible for determining valid moves at the current state.
+It will also keep a move log.
+"""
+
+import numpy as np
+
+
+class GameState():
+    def __init__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
---
+++ FakeFishMain.py
@@ -0,0 +1,76 @@
+"""
+This is our main driver file.
+It will be responsible for handling user input and displaying the current GameState object.
+"""
+
+import pygame as p
+import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8 #dimension of chess board (8x8)
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 #animations later on
+IMAGES = {}
+
+"""
+Initialize a global dictionary of images.
+This wlil be called exactly once in the main.
+"""
+
+def loadImages():
+    pieces = ["bB", "bK", "bN", "bP", "bQ", "bR", "wB", "wK", "wN", "wP", "wQ", "wR"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.smoothscale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+        # we can now retrieve images by key search in our dictonary 'IMAGES["wP"]'
+
+"""
+Main driver for our code
+Handle user input and updating graphics
+"""
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("White"))
+    gs = ChessEngine.GameState()
+    loadImages() #only do this once, before the while loop
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+"""
+Responsible for all the graphics within a current game state.
+"""
+def drawGameState(screen, gs):
+    drawBoard(screen) #draw squares on the board
+    #add piece highlighting or move suggestions
+    drawPieces(screen, gs.board) #draw pieces on top of those squares
+
+"""
+Draw the squares on the board. The top left square is always light
+"""
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c)%2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+"""
+Draw the pieces on the board using the current GameState.board
+"""
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--": #not an empty square
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+if __name__ == "__main__":
+    main()
